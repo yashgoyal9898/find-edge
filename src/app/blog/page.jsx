2996b8bbde886e52e943d7323d3db06c5e34d27f
yashgoyal9898 <yashgoyal9898@gmail.com>
2024-07-17 13:24:31 +0530

    api intergration

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Blogpostcard from '@/components/Blogpostcard/Blogpostcard';
 
-function page() {
-  return (
-    <div>page</div>
-  )
+async function getData() {
+  const res = await fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=63c4e5eca2554ed2a410f45266d556cc');
+  const data = await res.json();
+  return data.articles; // Return only the articles
 }
 
-export default page
+async function Page() {
+  const articles = await getData();
+  return (
+    <div className='home-latest-post'>
+      {articles.map((article) => (
+        <Blogpostcard key={article.url} article={article} />
+      ))}
+    </div>
+  );
+}
+
+export default Page;
--- src/components/Blogpostcard/Blogpostcard.js
+++ src/components/Blogpostcard/Blogpostcard.js
@@ -1,23 +1,32 @@
-import Image from "next/image"
-import "./Blogpostcard.css"
+import Image from "next/image";
+import "./Blogpostcard.css";
 
-function Blogpostcard() {
+function Blogpostcard({ article }) {
+  const {
+    title = "Default Title",
+    author = "Default Author",
+    publishedAt = "N/A",
+    urlToImage = "/image/blog-post-card.png",
+    description = "Default description",
+    source = { name: "General" }
+  } = article || {};
+
   return (
     <div className="bpc-par-wrapper">
-        <Image alt="" src="/image/blog-post-card.png" width="500" height="500"/>
-        <div className="bpc-contnt">
-            <span>Technology</span>
-            <h4 className="post-title">Hello</h4>
-            <div className="post-author-details">
-                <div>
-                    <Image alt="" src="/image/bpc-author.png" width="36" height="36"/>
-                    <span>Jason Francisco</span>
-                </div>
-                <span>August 20, 2022</span>
-            </div>
+      <Image alt={title} src="/image/blog-post-card.png" width={500} height={500} />
+      <div className="bpc-contnt">
+        <span>{source.name}</span>
+        <h4 className="post-title">{title}</h4>
+        <div className="post-author-details">
+          <div>
+            <Image alt={author} src="/image/bpc-author.png" width={36} height={36} />
+            <span>{author}</span>
+          </div>
+          <span>{new Date(publishedAt).toLocaleDateString() || publishedAt}</span>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Blogpostcard
+export default Blogpostcard;
