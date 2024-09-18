f072af57068ab2578fd17777cf4c2bc9f287a8c8
yashgoyal9898 <yashgoyal9898@gmail.com>
2024-09-18 19:34:53 +0530

    production error

--- src/app/terms-and-condition/page.js
+++ src/app/terms-and-condition/page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "../pages.css"
+import '../pages.css'
 
 const page = () => {
   return (
@@ -14,7 +14,7 @@ const page = () => {
 
             <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use Trademinia if you do not agree to take all of the terms and conditions stated on this page.</p>
 
-            <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Companys terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Clients needs in respect of provision of the Companys stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+            <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: &quot;Client&quot;, &quot;You&quot; and &quot;Your&quot; refers to you, the person log on this website and compliant to the Companys terms and conditions. &quot;The Company&quot;, &quot;Ourselves&quot;, &quot;We&quot;, &quot;Our&quot; and &quot;Us&quot;, refers to our Company. &quot;Party&quot;, &quot;Parties&quot;, or &quot;Us&quot;, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Clients needs in respect of provision of the Companys stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
 
         </div>
 
@@ -38,7 +38,7 @@ const page = () => {
                 <li>Redistribute content from Trademinia</li>
             </ul>
 
-            <p>This Agreement shall begin on the date hereof. Our Terms and Conditions were created with the help of the <a href="https://tools.bloggingqna.com/">Terms and Conditions Generator</a>.</p>
+            <p>This Agreement shall begin on the date hereof. Our Terms and Conditions were created with the help of the <a href='https://tools.bloggingqna.com/'>Terms and Conditions Generator</a>.</p>
 
             <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Trademinia does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Trademinia,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, Trademinia shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</p>
 
